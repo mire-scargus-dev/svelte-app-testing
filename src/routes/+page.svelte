20e955deadfa0830fc7797f729b8dfba0803cca6
Mireya Tapia <mire.t.m.dev@gmail.com>
2025-03-17 14:43:40 -0300

    titulo prueba
    descripcion prueba

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
-<script>
+<script lang="ts">
 	import Header from './Header.svelte';
+
+	let name = $state('Scott');
+	let status: 'OPEN' | 'ÇLOSED' = $state('OPEN');
+
 </script>
 
-<Header />
+<Header {name}/>
+
+<input type="text" bind:value={name}/>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,4 +1,16 @@
+<!-- primero va el script -->
 <script lang="ts">
+	let {
+		name //nombre del prop
+	}: {
+		name: string; //definicion de los tipos de las props
+	} = $props(); //no entiendo bien este tipo de nomenclatura
 </script>
 
-<h1>Hello from header</h1>
+<!-- luego el html -->
+<h1>Hello {name}</h1>
+<h2>{name.replaceAll('t', 'X')}</h2>
+
+<!-- y al final todo el css -->
+<style>
+</style>
