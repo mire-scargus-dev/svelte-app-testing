9a2cfbf921558e804cb9a7f042c9f4f4514f789e
carol <carol.sepulveda@dataobservatory.net>
2025-03-17 15:04:47 -0300

    📦feat: new navigation component
    - navigation with menu list
    - styles

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,15 @@
+<script>
+    let menuList = [
+        'Inicio',
+        'Menú 1',
+        'Menú 2'
+    ]
+</script>
+
+<nav class="flex gap-8 bg-green-700 text-white">
+    {#each menuList as menu}
+        <p class="hover:bg-blue-500 p-4 cursor-pointer">
+            {menu}
+        </p>
+    {/each}
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+	import Nav from '$lib/Nav.svelte';
 	import '../app.css';
 	let { children } = $props();
 </script>
 
 {@render children()}
+
+<Nav/>
