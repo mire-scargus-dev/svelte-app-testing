9ccbfc709f48d4924c1b41dce7f8d295cf512837
Mireya Tapia <mire.t.m.dev@gmail.com>
2025-03-24 15:22:41 -0300

    style: update style in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
-	import Nav from '$lib/Nav.svelte';
+	import Nav from '$lib/layout/Nav.svelte';
+	import BasicMap from '$lib/shared/BasicMap/BasicMap.svelte';
+	import Map from '$lib/shared/BasicMap/BasicMap.svelte';
+	import StyledMap from '$lib/shared/StyledMap.svelte';
 	import '../app.css';
 	let { children } = $props();
 </script>
 
 {@render children()}
 
-<Nav/>
+<Nav />
+<div class="flex gap-x-20 p-4 border border-red-500">
+	<BasicMap  />
+	<StyledMap />
+</div>
